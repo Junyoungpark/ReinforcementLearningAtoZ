4ce38b5e34b6317aac3f3bb88ca96a68c455cb8d
Junyoung Park <junyoungpark@kaist.ac.kr>
2020-07-04 14:12:13 +0900

    dqn exp

--- src/part5/run_DQN_exp.py
+++ src/part5/run_DQN_exp.py
@@ -10,14 +10,19 @@ from src.common.memory.memory import ReplayMemory
 from src.common.train_utils import to_tensor
 
 
-def run_DQN(target_update_interval: int,
+def run_DQN(batch_size: int,
+            target_update_interval: int,
             wandb_project: str):
-    # the hyperparameters are drawn from ""
+
+    # the hyperparameters are taken from 'minimalRL' implementation
+    # https://github.com/seungeunrho/minimalRL/blob/master/dqn.py
+    # the usage is under agreement with the original author.
+
     lr = 1e-4 * 5
-    batch_size = 16
-    gamma = 0.98
+    batch_size = batch_size
+    gamma = 1.0
     memory_size = 50000
-    total_eps = 10000
+    total_eps = 3000
     eps_max = 0.08
     eps_min = 0.01
     sampling_only_until = 2000
@@ -36,8 +41,8 @@ def run_DQN(target_update_interval: int,
                reinit=True,
                config=config)
 
-    qnet = MLP(4, 2, num_neurons=[128, 128])
-    qnet_target = MLP(4, 2, num_neurons=[128, 128])
+    qnet = MLP(4, 2, num_neurons=[128])
+    qnet_target = MLP(4, 2, num_neurons=[128])
     # initialize target network same as the main network.
     qnet_target.load_state_dict(qnet.state_dict())
 
@@ -72,11 +77,11 @@ def run_DQN(target_update_interval: int,
             if done:
                 break
 
-            if len(memory) >= sampling_only_until:
-                # train agent
-                sampled_exps = memory.sample(batch_size)
-                sampled_exps = prepare_training_inputs(sampled_exps)
-                agent.update(*sampled_exps)
+        if len(memory) >= sampling_only_until:
+            # train agent
+            sampled_exps = memory.sample(batch_size)
+            sampled_exps = prepare_training_inputs(sampled_exps)
+            agent.update(*sampled_exps)
 
         if n_epi % target_update_interval == 0:
             qnet_target.load_state_dict(qnet.state_dict())
@@ -93,8 +98,10 @@ def run_DQN(target_update_interval: int,
 
 if __name__ == '__main__':
     wandb_project = 'DQN'
-    target_update_interval = 5
+    target_update_interval = 1
+    batch_size = 256
 
     for i in range(10):
         run_DQN(wandb_project=wandb_project,
+                batch_size=batch_size,
                 target_update_interval=target_update_interval)
